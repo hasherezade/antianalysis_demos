3b2afffac7968ded21757a9a52468f233c3914f3
hasherezade <hasherezade@gmail.com>
2023-08-25 05:26:06 -0700

    [FEATURE] Added new techniques

--- main.cpp
+++ main.cpp
@@ -6,14 +6,103 @@
 #include "neutrino_checks.h"
 #include "kernelmode_antidbg.h"
 #include "procmon_check.h"
+#include "ntdll_undoc.h"
 
 //#define SINGLE_STEPPING_CHECK
 
+int main();
+
+bool checkProcessDebugFlags()
+{
+    // ProcessDebugFlags
+    const int ProcessDebugFlags = 0x1f;
+    auto _NtQueryInformationProcess = reinterpret_cast<decltype(&NtQueryInformationProcess)>(GetProcAddress(GetModuleHandleA("ntdll"), "NtQueryInformationProcess"));
+    // Other Vars
+    NTSTATUS Status;
+    DWORD NoDebugInherit = 0;
+
+    Status = _NtQueryInformationProcess(GetCurrentProcess(), ProcessDebugFlags, &NoDebugInherit, sizeof(DWORD), NULL);
+    std::cout << "ProcessDebugFlags: " << std::hex << NoDebugInherit << "\n";
+    
+    return (Status == 0 && NoDebugInherit == 0) ? true : false;
+}
+
+bool clearProcessDebugFlags()
+{
+    // ProcessDebugFlags
+    const int ProcessDebugFlags = 0x1f;
+
+    auto _NtSetInformationProcess = reinterpret_cast<decltype(&NtSetInformationProcess)>(GetProcAddress(GetModuleHandleA("ntdll"), "NtSetInformationProcess"));
+    // Other Vars
+    NTSTATUS Status;
+    DWORD NoDebugInherit = 1;
+
+    Status = _NtSetInformationProcess(GetCurrentProcess(), ProcessDebugFlags, &NoDebugInherit, sizeof(DWORD));
+    return (Status == 0);
+}
+
+#ifndef _WIN64
+bool exec_int2d()
+{
+    __try
+    {
+        __asm xor eax, eax;
+        __asm int 0x2d;
+        __asm nop;
+        return true;
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        return false;
+    }
+}
+#endif
+
+// from: https://anti-debug.checkpoint.com/techniques/process-memory.html#anti-step-over
+bool CheckForSpecificByte(BYTE cByte, PVOID pMemory, SIZE_T nMemorySize = 0)
+{
+    PBYTE pBytes = (PBYTE)pMemory;
+    for (SIZE_T i = 0; ; i++)
+    {
+        // Break on RET (0xC3) if we don't know the function's size
+        if (((nMemorySize > 0) && (i >= nMemorySize)) ||
+            ((nMemorySize == 0) && (pBytes[i] == 0xC3)))
+            break;
+
+        if (pBytes[i] == cByte)
+            return true;
+    }
+    return false;
+}
+
+bool IsCCSet()
+{
+    PVOID functionsToCheck[] = {
+        &main
+    };
+    for (auto funcAddr : functionsToCheck)
+    {
+        if (CheckForSpecificByte(0xCC, funcAddr))
+            return true;
+    }
+    return false;
+}
+
 int main()
 {
+    if (clearProcessDebugFlags()) {
+        std::cout << "Flag cleared!\n";
+    }
+    IsCCSet();
+#ifndef _WIN64
+    exec_int2d();
+#endif
     bool is_detected = false;
     antidbg_timer_check();
-
+    if (checkProcessDebugFlags()) {
+        is_detected = true;
+        std::cout << "[*] Debugger detected by ProcessDebugFlags!\n";
+    }
     if (exception_is_dbg()) {
         is_detected = true;
         std::cout << "[*] Debugger detected by Exception check!\n";
